7c53cfdd62282d789b09fb0c9512959cf0d87862
David <atunwadavidb@gmail.com>
2026-06-08 18:58:26 +0100

    p2p

--- hostname_to_ip_resolver/main.c
+++ hostname_to_ip_resolver/main.c
@@ -12,13 +12,13 @@ int main() {
     while(1) {
         char hostBuffer[100];
         
-        printf("Find IP address of: \n");
+        printf("Find IP address of: ");
         fgets(hostBuffer, sizeof(hostBuffer), stdin);
         hostBuffer[strcspn(hostBuffer, "\n")] = '\0';
         
         memset(&hints, 0, sizeof(hints)); //first we have to clear any garbage memory the OS might allocate to the struct
         
-        hints.ai_family = AF_INET; // what kind of IP address do i want to send and receive? Best left as unspecified
+        hints.ai_family = AF_INET6; // what kind of IP address do i want to send and receive? Best left as unspecified
         hints.ai_socktype = SOCK_STREAM; // how do i want the data to be sent? stream socket or datagram?
         
         // this is where we get the actual address information
@@ -28,7 +28,8 @@ int main() {
         // the next argument "80" is the port. Port 80 is the port for all unencrypted web traffic. 443 is for encrypted traffic (HTTPS). We are not actually sending any data here. Just a database lookup.
         // &hints is the memory address of the addrinfo struct. hints already exists in the memory because we initialised it earlier. All getaddrinfo is doing is reading the "hints" that we gave it and using it as criteria for its DNS search.
         
-        // *res is a pointer variable. right now it probably holds garbage but it is going to hold the memory address of a addrinfo struct when we pass it to getaddrinfo. we pass &res and not *res because we want to pass the function the memory address of where it can write the memory address of the addrinfo struct it gets from the lookup. it has to be done like this because functions in c can only return one thing, being whether the call was successful or not which is why the function is set to int status.
+        // *res is a pointer variable. right now it probably holds garbage but it is going to hold the memory address of a addrinfo struct when we pass it to getaddrinfo. we pass &res and not *res because we want to pass the function the memory address of where it can write the memory address of the addrinfo struct it gets from the lookup. *res would be dereferencing the struct addrinfo and would probably return the garbage data.
+        //it has to be done like this because functions in c can only return one thing, being whether the call was successful or not which is why the function is set to int status.
         
         int status = getaddrinfo(hostBuffer, NULL , &hints, &res);
         
@@ -37,15 +38,15 @@ int main() {
             return 1;
         }
         
-        char ip_string[46];
-        memset(ip_string, 0, sizeof(ip_string));
+        char ipv6_string[INET6_ADDRSTRLEN];
         
-        struct sockaddr_in  *ipv4 = (struct sockaddr_in *)res->ai_addr;
-        inet_ntop(AF_INET, &(ipv4->sin_addr), ip_string, sizeof(ip_string));
+        struct sockaddr_in6* ipv6 =  (struct sockaddr_in6* )(res->ai_addr);
+        inet_ntop(AF_INET6, &(ipv6->sin6_addr), ipv6_string, INET6_ADDRSTRLEN);
         
-        printf("The ip address for %s is %s\n", hostBuffer, ip_string);
+        printf("This is the ipv6 address for %s : %s\n", hostBuffer, ipv6_string);
         
         freeaddrinfo(res);
+        
     }
     
     return 0;
